043005f89ac3a1986063c081d6e9b504b70b7202
anshul <anshul.mankar@webol.co.uk>
2025-06-11 11:41:39 +0530

    added utility with add method along with validation

---
+++ string-calculator/src/utils/stringCalculator.js
@@ -0,0 +1,20 @@
+function add(numbers) {
+  if (!numbers) return 0;
+
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split('\n');
+    delimiter = new RegExp(parts[0].slice(2));
+    numbers = parts[1];
+  }
+
+  const nums = numbers.split(delimiter).map(Number);
+  const negatives = nums.filter(n => n < 0);
+  if (negatives.length) {
+    throw new Error(`negative numbers not allowed ${negatives.join(',')}`);
+  }
+
+  return nums.reduce((sum, n) => sum + n, 0);
+}
+
+export default add;
